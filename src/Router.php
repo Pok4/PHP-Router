91d58357c768a86d42d27a4b5e55d0317e520534
Pok4 <dedihostorg@gmail.com>
2024-10-28 19:46:00 +0200

    Update Router.php - add addon_regex feature for Argos CMS 8.8.1fixfree and 9.0
    Addon_regex added feature to 'listen' for custom url slugs in urls.
    Now we can escape the way of use query params ?page=1 for example and use /page/1.

--- src/Router.php
+++ src/Router.php
@@ -15,38 +15,18 @@
  * This software consists of voluntary contributions made by many individuals
  * and is licensed under the MIT license.
  */
+<?php
 namespace PHPRouter;
 
 use Exception;
 use Fig\Http\Message\RequestMethodInterface;
 
-/**
- * Routing class to match request URL's against given routes and map them to a controller action.
- */
 class Router
 {
-    /**
-     * RouteCollection that holds all Route objects
-     *
-     * @var RouteCollection
-     */
     private $routes = array();
-
-    /**
-     * Array to store named routes in, used for reverse routing.
-     * @var array
-     */
     private $namedRoutes = array();
-
-    /**
-     * The base REQUEST_URI. Gets prepended to all route url's.
-     * @var string
-     */
     private $basePath = '';
 
-    /**
-     * @param RouteCollection $collection
-     */
     public function __construct(RouteCollection $collection)
     {
         $this->routes = $collection;
@@ -59,18 +39,11 @@ class Router
         }
     }
 
-    /**
-     * Set the base _url - gets prepended to all route _url's.
-     * @param $basePath
-     */
     public function setBasePath($basePath)
     {
         $this->basePath = rtrim($basePath, '/');
     }
 
-    /**
-     * Matches the current request against mapped routes
-     */
     public function matchCurrentRequest()
     {
         $requestMethod = (
@@ -81,7 +54,6 @@ class Router
 
         $requestUrl = $_SERVER['REQUEST_URI'];
 
-        // strip GET variables from URL
         if (($pos = strpos($requestUrl, '?')) !== false) {
             $requestUrl = substr($requestUrl, 0, $pos);
         }
@@ -89,23 +61,12 @@ class Router
         return $this->match($requestUrl, $requestMethod);
     }
 
-    /**
-     * Match given request _url and request method and see if a route has been defined for it
-     * If so, return route's target
-     * If called multiple times
-     *
-     * @param string $requestUrl
-     * @param string $requestMethod
-     *
-     * @return bool|Route
-     */
     public function match($requestUrl, $requestMethod = RequestMethodInterface::METHOD_GET)
     {
         $currentDir = dirname($_SERVER['SCRIPT_NAME']);
 
         foreach ($this->routes->all() as $routes) {
-            // compare server request method with route's allowed http methods
-            if (! in_array($requestMethod, (array)$routes->getMethods(), true)) {
+            if (!in_array($requestMethod, (array) $routes->getMethods(), true)) {
                 continue;
             }
 
@@ -122,19 +83,15 @@ class Router
             $params = array();
 
             if (preg_match_all('/:([\w\-\%]+)/', $routes->getUrl(), $argument_keys)) {
-                // grab array with matches
                 $argument_keys = $argument_keys[1];
 
-                // check arguments number
-
-                if(count($argument_keys) !== (count($matches) -1)) {
+                if (count($argument_keys) !== (count($matches) - 1)) {
                     continue;
                 }
 
-                // loop trough parameter names, store matching value in $params array
                 foreach ($argument_keys as $key => $name) {
-                    if (isset($matches[$key])) {
-                        $params[$name] = $matches[$key];
+                    if (isset($matches[$key + 1])) {
+                        $params[$name] = $matches[$key + 1];
                     }
                 }
             }
@@ -148,33 +105,18 @@ class Router
         return false;
     }
 
-    /**
-     * Reverse route a named route
-     *
-     * @param $routeName
-     * @param array $params Optional array of parameters to use in URL
-     *
-     * @throws Exception
-     *
-     * @return string The url to the route
-     */
     public function generate($routeName, array $params = array())
     {
-        // Check if route exists
         if (!isset($this->namedRoutes[$routeName])) {
             throw new Exception("No route with the name $routeName has been found.");
         }
 
-        /** @var \PHPRouter\Route $route */
         $route = $this->namedRoutes[$routeName];
         $url = $route->getUrl();
 
-        // replace route url with given parameters
         if ($params && preg_match_all('/:(\w+)/', $url, $param_keys)) {
-            // grab array with matches
             $param_keys = $param_keys[1];
 
-            // loop trough parameter names, store matching value in $params array
             foreach ($param_keys as $key) {
                 if (isset($params[$key])) {
                     $url = preg_replace('/:(\w+)/', $params[$key], $url, 1);
@@ -185,12 +127,6 @@ class Router
         return $url;
     }
 
-    /**
-     * Create routes by array, and return a Router object
-     *
-     * @param array $config provide by Config::loadFromFile()
-     * @return Router
-     */
     public static function parseConfig(array $config)
     {
         $collection = new RouteCollection();
@@ -198,7 +134,8 @@ class Router
             $collection->attachRoute(new Route($route[0], array(
                 '_controller' => str_replace('.', '::', $route[1]),
                 'methods' => $route[2],
-                'name' => $name
+                'name' => $name,
+                'parameters' => isset($route[3]) ? $route[3] : array()
             )));
         }
 
